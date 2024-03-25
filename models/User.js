d53fdffb44544066a2ddcd4266743660cc2e4251
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-03-24 20:55:39 -0600

    feat: Crea el modelo de user

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+});
+
+export default model('User', userSchema);
