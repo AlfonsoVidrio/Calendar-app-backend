ef9b8b9489afc34f234bec09379a6e124a19bba2
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-03-26 15:02:39 -0600

    feat: Completa el controlador para updateEvent

--- controllers/events.js
+++ controllers/events.js
@@ -1,3 +1,4 @@
+import { trusted } from "mongoose";
 import Event from "../models/Event.js";
 
 export const getEvent = async(req, res) => {
@@ -30,11 +31,47 @@ export const addEvent = async (req, res) => {
     }
 }
 
-export const updateEvent = (req, res) => {
-    return res.status(200).json({
-        ok: true,
-        msg: 'actualizar eventos'
-    })
+export const updateEvent =  async (req, res) => {
+    
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try{
+
+        const event = await Event.findById( eventId );
+
+        if ( !event ){
+            res.status(404).json({
+                ok: false,
+                msg: "No existe el evento."
+            })
+        }
+        
+        if ( event.user.toString() !== uid ) return res.status(401).json({
+            ok: false,
+            msg: 'No esta autorizado para editar este evento.'
+        })
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+        
+        const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent, { new: true} )
+
+        return res.status(200).json({
+            ok: true,
+            event: updatedEvent
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: "No se pudo actualizar el evento."
+        })
+    }
+
+
 }
 
 export const deleteEvent = (req, res) => {
