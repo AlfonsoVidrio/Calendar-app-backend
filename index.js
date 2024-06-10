f950775ed77aba16d4a2eb4db891bdc95f21c9b7
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-06-09 22:23:05 -0600

    redirección de rutas no manejadas

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 import auth from './routes/auth.js'
 import events from './routes/events.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
@@ -25,6 +31,15 @@ app.use(express.json());
 
 app.use('/api/auth', auth);
 app.use('/api/events', events);
+
+// Todas las rutas no manejadas explícitamente serán redirigidas al index.html
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), function(err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 
 // escuchar
 app.listen(process.env.PORT, () => {
